b79c59616434cacd24a83793e0a0e19e18204deb
Diana Vidal <dvidaal@gmail.com>
2023-02-13 22:55:28 +0100

    create Form component and add css

---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,17 @@
+const Form = (): JSX.Element => {
+  return (
+    <div className="container-form">
+      <input
+        className="form-input"
+        placeholder="Create Task"
+        aria-label="Field to create a task"
+      />
+
+      <button type="submit" aria-label="Create button">
+        Create
+      </button>
+    </div>
+  );
+};
+
+export default Form;
