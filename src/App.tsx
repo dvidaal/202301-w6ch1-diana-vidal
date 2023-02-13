cc0b8f5e3e8e9d0b37c32cbedebc54b1f0b2a99f
Diana Vidal <dvidaal@gmail.com>
2023-02-13 22:48:04 +0100

    create header in  app and add css

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 
 import "./App.css";
+import Form from "./components/Form/Form";
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="container">
+      <header className="main-header">
+        <h1>To Do list ✅</h1>
+        <Form />
+      </header>
+    </div>
+  );
 }
 
 export default App;
